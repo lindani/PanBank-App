f73002ff2f96ab14133423e8b6cbcf10ad7ef0b8
lindani <panilindani02@gmail.com>
2024-05-19 12:40:59 +0200

    feat: implement complete layout with sidebar, rightbar and dashboard components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Dashboard = () => {
-  const isLoggedIn = {firstName: 'John'}
+  const isLoggedIn = {firstName: 'John', lastName: 'Pani', email: 'panilindani@gmail.com'}
   return (
     <section className='home'>
       <div className='home-content'>
@@ -21,7 +22,13 @@ const Dashboard = () => {
             totalCurrentBalance = {1111.50}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar
+        user={isLoggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 1245.69}, {currentBalance: 600.00}]}
+      />
     </section>
   )
 }
---
+++ components/TotalBalanceBox.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import AnimatedCounter from './AnimatedCounter'
+import DoughnutChart from './DoughnutChart'
+
+function TotalBalanceBox({ accounts = [], totalBanks, totalCurrentBalance }: TotalBalanceBoxProps) {
+  return (
+    <section className='total-balance'>
+        <div className='total-balance-chart'>
+						<DoughnutChart accounts={accounts}/>
+        </div>
+        <div className='flex flex-col gap-6'>
+          <h2 className='header-2'>Number of banks: {totalBanks}</h2>
+					<div className='flex flex-col gap-2'>
+						<p className='total-balance-label'> Total Current Balance</p>
+						<p className='total-current-balance flex-center gap-2'>
+							<AnimatedCounter amount = {totalCurrentBalance}/>
+						</p>
+					</div>
+        </div>
+    </section>
+  )
+}
+
+export default TotalBalanceBox
